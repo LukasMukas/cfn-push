4418705138764a2001470e755420f009d042dcdc
EC2 Default User <ec2-user@ip-172-31-22-169.eu-west-2.compute.internal>
2019-09-13 08:56:11 +0000

    Added the possibility of linting the file without pushing it to a bucket

--- run.sh
+++ run.sh
@@ -4,12 +4,13 @@ helpFunction()
 {
    echo "Lint a cfn script and then upload it to an s3 bucket"
    echo "Usage: $0 {file} bucket"
+   echo "To lint the template without uploading it do not pass a bucket parameter"
    exit 1 # Exit script after printing help
 }
 
-if [ -z "$2" ]
+if [ -z "$1" ]
 then 
-  echo "You must provide a file and a bucket"
+  echo "You must provide a file and optionally a bucket"
   exit 1
 fi
 
@@ -25,6 +26,12 @@ then
       echo "$fail"
     fi
     echo "nag passed"
+    if [ -z $2 ];
+    then 
+      echo "No bucket was provided"
+      exit 0
+    fi
+    echo "Uploading the template to $2" 
     aws s3 cp $1 $2
   else 
     echo "$fail"
